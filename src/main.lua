f040dccc55877a7de350be71e59a1d684e695a22
TheGrumpyGameDev <thegrumpygamedev@gmail.com>
2026-03-16 09:33:36 -0500

    stuff

---
+++ src/main.lua
@@ -0,0 +1,50 @@
+if arg[#arg] == "debug" then
+    require("lldebugger").start()
+end
+local mousemap = require("mousemap")
+local constants = require("constants")
+
+local lightTile
+local darkTile
+local cursorTile
+local cursorX = 0
+local cursorY = 0
+
+function love.load(args)
+    lightTile = love.graphics.newImage("assets/images/lighttile.png")
+    darkTile = love.graphics.newImage("assets/images/darktile.png")
+    cursorTile = love.graphics.newImage("assets/images/cursor.png")
+    mousemap.load()
+end
+
+function plotXY(x,y)
+    return constants.SCREEN_WIDTH / 2 - constants.TILE_WIDTH / 2 + (x - y) * (constants.TILE_WIDTH / 2) , (x + y) * (constants.TILE_HEIGHT / 2)
+end
+
+function love.draw()
+    for x = 0, 7 do
+        for y = 0, 7 do
+            local tile = lightTile
+            if (x+y)%2==1 then
+                tile = darkTile
+            end
+            local plotX, plotY = plotXY(x,y)
+            love.graphics.draw(tile, plotX, plotY)
+            if x == cursorX and y == cursorY then
+                love.graphics.draw(cursorTile, plotX, plotY)
+            end
+        end
+    end
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+    cursorX, cursorY = mousemap.mapXY(x,y)
+end
+--80x80
+--https://game-icons.net/1x1/skoll/chess-knight.html
+--https://game-icons.net/1x1/skoll/chess-bishop.html
+--https://game-icons.net/1x1/skoll/chess-pawn.html
+--https://game-icons.net/1x1/sbed/flake.html
+--https://game-icons.net/1x1/skoll/spiked-bat.html
+--https://game-icons.net/1x1/delapouite/wood-club.html
+--https://game-icons.net/1x1/delapouite/flanged-mace.html
---
+++ src/mousemap.lua
@@ -0,0 +1,64 @@
+local constants = require("constants")
+local M = {}
+local MOUSEMAP_IMAGE_FILENAME = "assets/images/mousemap.png"
+local UPPER_LEFT = "ul"
+local UPPER_RIGHT = "ur"
+local LOWER_LEFT = "ll"
+local LOWER_RIGHT = "lr"
+local CENTER = "c"
+local MINIMUM_X = 0
+local MAXIMUM_X = 7
+local MINIMUM_Y = 0
+local MAXIMUM_Y = 7
+function clamp(value, minimum, maximum)
+    return math.max(minimum, math.min(value, maximum))
+end
+function M.load()
+    M.mouseMap = {}
+    local mouseMapImageData = love.image.newImageData(MOUSEMAP_IMAGE_FILENAME)
+    local width = mouseMapImageData:getWidth();
+    local height = mouseMapImageData:getHeight();
+    local ulr,ulg,ulb = mouseMapImageData:getPixel(0,0)
+    local urr,urg,urb = mouseMapImageData:getPixel(width-1,0)
+    local llr,llg,llb = mouseMapImageData:getPixel(0,height-1)
+    local lrr,lrg,lrb = mouseMapImageData:getPixel(width-1,height-1)
+    for x = 0, width - 1 do
+        M.mouseMap[x]={}
+        for y = 0, height - 1 do
+            local r, g, b = mouseMapImageData:getPixel(x,y)
+            if r == ulr and g == ulg and b == ulb then
+                M.mouseMap[x][y]=UPPER_LEFT
+            elseif r == urr and g == urg and b == urb then
+                M.mouseMap[x][y]=UPPER_RIGHT
+            elseif r == llr and g == llg and b == llb then
+                M.mouseMap[x][y]=LOWER_LEFT
+            elseif r == lrr and g == lrg and b == lrb then
+                M.mouseMap[x][y]=LOWER_RIGHT
+            else
+                M.mouseMap[x][y]=CENTER
+            end
+        end
+    end
+end
+function M.mapXY(x,y)
+    local mapX, mapY = cursorX, cursorY
+    x = x - constants.SCREEN_WIDTH / 2 + constants.TILE_WIDTH / 2
+    local gridX = math.floor(x / constants.TILE_WIDTH)
+    local gridY = math.floor(y / constants.TILE_HEIGHT)
+    local remainderX = x % constants.TILE_WIDTH
+    local remainderY = y % constants.TILE_HEIGHT
+    mapX = gridY + gridX
+    mapY = gridY - gridX
+    local corner = M.mouseMap[remainderX][remainderY]
+    if corner == UPPER_LEFT then
+        mapX = mapX - 1
+    elseif corner == UPPER_RIGHT then
+        mapY = mapY - 1
+    elseif corner == LOWER_LEFT then
+        mapY = mapY + 1
+    elseif corner == LOWER_RIGHT then
+        mapX = mapX + 1
+    end
+    return clamp(mapX,MINIMUM_X,MAXIMUM_X), clamp(mapY,MINIMUM_Y,MAXIMUM_Y)
+end
+return M
